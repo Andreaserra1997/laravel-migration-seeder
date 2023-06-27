2cc763b601c2c6d57a33d58c07b73253df56c0d1
Andreaserra1997 <serra.andrea1997@gmail.com>
2023-06-27 16:15:16 +0200

    add migration

---
+++ database/migrations/2023_06_27_134636_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company', 50);
+            $table->string('starting_station', 50);
+            $table->string('end_station', 50);
+            $table->string('departure_time', 5);
+            $table->string('arrival_time', 5);
+            $table->string('train_code', 5);
+            $table->tinyInteger('number_coaches');
+            $table->boolean('on_time');
+            $table->boolean('deleted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
